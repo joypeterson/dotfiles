52563b0458328b5a742f1504a5cbfe424b4c99e2
Joy Peterson <1217200+joypeterson@users.noreply.github.com>
2021-10-29 12:56:40 -0600

    Add homebrew setup

---
+++ run_once_01-homebrew-install.sh
@@ -0,0 +1,16 @@
+
+   
+#!/bin/bash
+#
+# Homebrew
+#
+# This installs Homebrew if it is not already installed
+
+# Check for Homebrew
+if test ! $(which brew)
+then
+  echo "  Installing Homebrew for you."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+exit 0
